280b65f8a0d653b6e926c18ce81aa6a2cd0ab6e2
Andrew Gerst <gerst20051@gmail.com>
2020-11-26 21:55:12 -0800

    react deployment updates

--- react-address-manager/src/config.js
+++ react-address-manager/src/config.js
@@ -4,8 +4,8 @@ const dev = {
 };
 
 const prod = {
-  API_URL: 'https://q8sxd5phn7.execute-api.us-east-1.amazonaws.com/dev',
-  SWAGGER_URL: 'https://q8sxd5phn7.execute-api.us-east-1.amazonaws.com/dev/swagger',
+  API_URL: 'https://6tpeua70ga.execute-api.us-east-1.amazonaws.com/dev',
+  SWAGGER_URL: 'https://6tpeua70ga.execute-api.us-east-1.amazonaws.com/dev/swagger',
 };
 
 const config = process.env.NODE_ENV === 'production' ? prod : dev;
